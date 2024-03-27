4a270d651cd50fbede3a8eb529e7733ca11d62da
Ayush Nainwal <aayu@Ayushs-MacBook-Air.local>
2024-03-28 01:01:49 +0530

    Resolved some issues again

--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -1,9 +1,10 @@
 import SectionHeader from "../SectionHeader";
 import EventBox from "./EventBox";
-
+import { data } from "../../../data";
 
 const Events = async() => {
 
+  const event = data.events;
     
   return <section className="section" id="tours">
     <div className="container mx-auto text-white">
